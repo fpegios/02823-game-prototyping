644f18a92455fb15ec2ac8ac4bb66fa32cf03dfa
FPazzini <fabio.pazzini@icloud.com>
2017-10-17 11:43:46 +0200

    new Player Controller script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float maxSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D rb;
+    
+    // Use this for initialization
+    void Start()
+    {
+        // Getting the Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Updating the velocity property of the object
+        rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+
+        // Getting the input - if Space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Then we update the y value of the velocity property to jump
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+}
