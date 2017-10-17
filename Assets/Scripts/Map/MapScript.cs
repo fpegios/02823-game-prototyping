e8e4f78e129198a560e5145a16d95efcc2bb7e5d
Fotis Pegios <fotispeg@gmail.com>
2017-10-17 14:12:22 +0200

    #15 add player navigation between level milestones

--- Assets/Scripts/Map/MapScript.cs
+++ Assets/Scripts/Map/MapScript.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class MapScript : MonoBehaviour {
 
+	public int currentLevel;
+	public int clickedLevel;
+	public int maxAvailableLevel;
+	public Vector3 currentPosition;
+	public Vector3 targetPosition;
+	public float playerSpeed;
+
 	void Start () {
 		Debug.Log("STARTED");
+
+		// set current level
+		currentLevel = 1;
+
+		// set max available level
+		maxAvailableLevel = 3;
+		
+		// define player's speed
+		playerSpeed = 0.05f;
+
+		// get current level position
+		currentPosition = GameObject.Find("Level_" + currentLevel).transform.position;
+
+		// move player to the current level position
+		GameObject.Find("Player").transform.position = currentPosition;
+
+		// set target position
+		targetPosition = currentPosition;
 	}
 	
 	void Update () {
+
+		// if target position is different from the current one
+		// move the player gradually to it per frame
+		if (targetPosition != currentPosition) {
+			// update current position by moving it towards the targeted one
+			currentPosition = Vector3.MoveTowards(currentPosition, targetPosition, playerSpeed);
+
+			// update player position
+			GameObject.Find("Player").transform.position = currentPosition;
+		}
 		
 		// Event on mouse click
 		if (Input.GetMouseButtonDown (0)) {
@@ -18,11 +53,20 @@ public class MapScript : MonoBehaviour {
 			// get the hit sprite
 			RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
 			
-			// Check if user clicked a sprite
+			// check if user clicked a sprite
 			if (hit.collider != null) {
-				Debug.Log ("You Clicked: " + hit.collider.name);
+				// get clicked level
+				clickedLevel = int.Parse(hit.collider.name.Substring(hit.collider.name.Length-1, 1));
+				
+				// check if the clicked level is available
+				if ( clickedLevel <= maxAvailableLevel) {
+					// update current level
+					currentLevel = clickedLevel;
 
-				// ... paste here the code for transition to another scene
+					// update target position
+					targetPosition = GameObject.Find("Level_" + currentLevel).transform.position;
+				}
+				
 			}
         }
 	}
