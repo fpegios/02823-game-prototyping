2c941faa8ebe9778d246e5095457a586fd49643c
Fotis Pegios <fotispeg@gmail.com>
2017-10-29 17:17:23 +0100

    create moving platform script #52

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public float frequency = 1.0f;
+	public float amplitude = 3;
+	private Vector3 originalPosition;
+	void Start () {
+		originalPosition = transform.position;
+	}
+	
+	void Update () {
+		// X = Acos(wt) = Acos(2πf)
+		Debug.Log(transform.position);
+		transform.position = new Vector3(originalPosition.x + Mathf.Sin(Time.time * frequency) * amplitude, originalPosition.y, originalPosition.z);
+	}
+}
