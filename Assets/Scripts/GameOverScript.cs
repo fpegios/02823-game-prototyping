b2adaa132e4d1882fce9dc1a8e6c8e1eb6d3500b
Fotis Pegios <fotispeg@gmail.com>
2017-11-14 14:31:46 +0100

    #59

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour {
 
 	private enum GameOverMenu {Restart, MainMenu};
 	private GameOverMenu currentChoice;
 	private Vector3 indicatorPosition;
-	
 	private Text text;
+	private string mapSceneRelativePath = "Scenes/Other/MapScene";
+	private string levelSceneRelativePath = "Scenes/Levels/";
+	private SceneController sceneController;
+
+	void Awake () {
+		sceneController = FindObjectOfType<SceneController>();
+		if(!sceneController)
+			throw new UnityException("Scene Controller could not be found, ensure that it exists in the Persistent scene.");
+	}
+
 	void Start () {
 		// initiaize current choice to restart
 		currentChoice = GameOverMenu.Restart;
@@ -21,8 +31,16 @@ public class GameOverScript : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space)) {	
 			if (currentChoice == GameOverMenu.Restart) {
 				Debug.Log("RESTART THE LEVEL!");
+				// hide the game over menu
+				// get active scene name and reload it
+				this.gameObject.SetActive(false);
+				Scene activeScene = SceneManager.GetActiveScene();
+				sceneController.FadeAndLoadScene(levelSceneRelativePath + activeScene.name);
 			} else if (currentChoice == GameOverMenu.MainMenu) {
 				Debug.Log("GO TO MAIN MENU!");
+				// hide the game over menu and load map scene
+				this.gameObject.SetActive(false);
+				sceneController.FadeAndLoadScene(mapSceneRelativePath);
 			}
         }
 
