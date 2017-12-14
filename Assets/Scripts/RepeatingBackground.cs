d5063129c84e3d9cc7ee382d43bf4982d142ab0c
Fotis Pegios <fotispeg@gmail.com>
2017-12-15 00:27:02 +0200

    #88 fix background issue when rewind
    backward background when respawn position is with no background

--- Assets/Scripts/RepeatingBackground.cs
+++ Assets/Scripts/RepeatingBackground.cs
@@ -7,6 +7,7 @@ public class RepeatingBackground : MonoBehaviour {
 	private SpriteRenderer spriteRenderer;
 	private float groundHorizontalLength;
 	private GameObject player;
+	private bool respawnPositionChecked = false;
 
 	void Start () {
 		spriteRenderer = GetComponent<SpriteRenderer> ();
@@ -15,12 +16,27 @@ public class RepeatingBackground : MonoBehaviour {
 	}
 	
 	void Update () {
-		if (player.transform.position.x - transform.position.x > (groundHorizontalLength)) {
-			RepositionBackground();
+		Debug.Log(PlayerController.respawnPosition);
+		if (!PlayerController.isRespawning) {
+			if (player.transform.position.x - transform.position.x > (groundHorizontalLength)) {
+				RepositionBackground();
+			}
+			respawnPositionChecked = false;
+		} else {
+			if (!respawnPositionChecked) {
+				if (PlayerController.respawnPosition.x < transform.position.x - (1.5f * groundHorizontalLength)) {
+					BackwardRepositionBackground();
+				}
+				respawnPositionChecked = true;
+			}
 		}
 	}
 
 	private void RepositionBackground() {
 		transform.position = new Vector3(transform.position.x + groundHorizontalLength *1.99f, transform.position.y, transform.position.z);
+	}
+
+	private void BackwardRepositionBackground() {
+		transform.position = new Vector3(transform.position.x - groundHorizontalLength *1.99f, transform.position.y, transform.position.z);
 	}
 }
